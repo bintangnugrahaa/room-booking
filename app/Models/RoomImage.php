d3992d7a080f21610b88c78147f55b4e73e6a252
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-27 15:50:20 +0700

    define columns and fillable attributes (part 2)

--- app/Models/RoomImage.php
+++ app/Models/RoomImage.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RoomImage extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['room_id', 'image'];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
