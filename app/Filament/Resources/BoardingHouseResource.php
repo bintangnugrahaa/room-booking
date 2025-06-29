f98f28e5185262839d34333a8171caaadc9f2be3
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-29 19:12:36 +0700

    add CRUD for Boarding House Testing Data

--- app/Filament/Resources/BoardingHouseResource.php
+++ app/Filament/Resources/BoardingHouseResource.php
@@ -128,13 +128,19 @@ class BoardingHouseResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('city.name'),
+                Tables\Columns\TextColumn::make('category.name'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\ImageColumn::make('thumbnail'),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
