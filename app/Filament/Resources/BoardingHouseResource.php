efa2109d5b9dd4cafd572852c4c837640f6f70b0
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-29 18:57:36 +0700

    add CRUD for Boarding House Rooms

--- app/Filament/Resources/BoardingHouseResource.php
+++ app/Filament/Resources/BoardingHouseResource.php
@@ -67,6 +67,7 @@ class BoardingHouseResource extends Resource
                         Forms\Components\Tabs\Tab::make('Bonus Ngekos')
                             ->schema([
                                 Forms\Components\Repeater::make('bonuses')
+                                    ->relationship('bonuses')
                                     ->schema([
                                         Forms\Components\FileUpload::make('image')
                                             ->image()
@@ -81,8 +82,43 @@ class BoardingHouseResource extends Resource
                                     ])
                             ]),
 
-                        Forms\Components\Tabs\Tab::make('Tab 3')
-                            ->schema([]),
+                        Forms\Components\Tabs\Tab::make('Kamar')
+                            ->schema([
+                                Forms\Components\Repeater::make('rooms')
+                                    ->relationship('rooms')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('name')
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('room_type')
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('square_feet')
+                                            ->numeric()
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('capacity')
+                                            ->numeric()
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('price_per_month')
+                                            ->numeric()
+                                            ->prefix('IDR')
+                                            ->required(),
+
+                                        Forms\Components\Toggle::make('is_available')
+                                            ->required(),
+
+                                        Forms\Components\Repeater::make('roomImages')
+                                            ->relationship('roomImages')
+                                            ->schema([
+                                                Forms\Components\FileUpload::make('image')
+                                                    ->image()
+                                                    ->directory('rooms')
+                                                    ->required(),
+                                            ]),
+                                    ])
+                            ]),
                     ])
                     ->columnSpanFull(),
             ]);
