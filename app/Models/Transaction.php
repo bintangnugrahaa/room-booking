c0ce23896832805090bf33346aeca7183636c355
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-29 19:16:22 +0700

    implement soft delete

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['code', 'boarding_house_id', 'room_id', 'name', 'email', 'phone_number', 'payment_method', 'payment_status', 'start_date', 'duration', 'total_amount', 'transaction_date'];
 
