ceb065ae36ddbfce4396a22e4955bf97c1c4f4d9
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-07-01 10:46:47 +0700

    create repositories

--- app/Interfaces/CategoryRepositoryInterface.php
+++ app/Interfaces/CategoryRepositoryInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Interfaces;
+
 interface CategoryRepositoryInterface
 {
     public function getAllCategories();
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\CityRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Repositories\CityRepository;
+use App\Repositories\CategoryRepository;
+use App\Repositories\BoardingHouseRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -11,7 +17,9 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CityRepositoryInterface::class, CityRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(BoardingHouseRepositoryInterface::class, BoardingHouseRepository::class);
     }
 
     /**
