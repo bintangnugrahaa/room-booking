39c63ad0dea0b3e8e033e0d0a196bf6dabdd1af9
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-07-06 02:11:38 +0700

    redirect to booking success page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::post('/kos/booking/{slug}/information/save', [BookingController::class, '
 Route::get('/kos/booking/{slug}', [BookingController::class, 'booking'])->name('booking');
 Route::get('/kos/booking/{slug}/checkout', [BookingController::class, 'checkout'])->name('booking.checkout');
 Route::post('/kos/booking/{slug}/payment', [BookingController::class, 'payment'])->name('booking.payment');
+Route::get('/booking-success', [BookingController::class, 'success'])->name('booking.success');
